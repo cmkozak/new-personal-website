aaab36e5acad3e74ba3e6e127ea8a1db8e7073e2
Michael Kozak <charlesmichaelk@gmail.com>
2021-06-01 19:55:43 -0500

    add projects to local data

--- src/store/index.js
+++ src/store/index.js
@@ -1,46 +1,7 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {
-    projectdata: {
-      projects: [
-        {
-          name: "Old Portfolio Website",
-          languages: ["React"],
-          img: require("@/assets/images/old-portfolio-website.png"),
-          githublink: "https://github.com/cmkozak/personal-website",
-          demolink: "",
-          inprogress: false,
-          expanded: false,
-          info: `In my previous portfolio, my goal was to expand my knowledge on React and Javascript by creating a single page website that features a large amount of animations and transitions.<br/>
-      Before starting this website I had never used React before but I knew that I wanted to make my portfolio interactive so I started learning React a few months prior to creating this website.<br/>
-      I eventaully scrapped the project in early 2019.`,
-        },
-        {
-          name: "Currency Converter",
-          languages: ["Javascript"],
-          img: require("@/assets/images/currency-converter.png"),
-          githublink: "https://github.com/cmkozak/currency-converter",
-          demolink: "",
-          inprogress: false,
-          expanded: false,
-          info: `"I created this project mostly to familiarize myself with using HTTP GET requests. Creating a simple single-page website that converts from 12 different currencies types to many different currencies used all over the world.<br/>
-          The website I use to get the information from is http://www.floatrates.com/json-feeds.html which offers free currency converters from many different countries updated daily.</br>
-          The project doesn’t look very pretty and I didn’t spend a lot of time on the design parts of it. I mostly just wanted to quickly throw some design together and get used to utilizing API requests."`,
-        },
-        {
-          name: "New Portfolio Website",
-          languages: ["Vue"],
-          img: "",
-          githublink: "https://github.com/cmkozak/new-personal-website",
-          demolink: "",
-          inprogress: true,
-          expanded: false,
-          info: `""`,
-        },
-      ],
-    },
-  },
+  state: {},
   mutations: {},
   actions: {},
   modules: {},
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -4,7 +4,7 @@
   <div class="content projects">
     <div class="card-container">
       <div
-        v-for="(projects, idx) in projectdata.projects"
+        v-for="(projects, idx) in projects"
         :key="projects"
         class="card"
         :id="`${idx}`"
@@ -13,7 +13,18 @@
         <div class="top-card-content">
           <img :src="`${projects.img}`" />
           <span v-if="projects.expanded === true" class="expanded side">
-            <p>{{ projects.languages[0] }}</p>
+            <p class="language">{{ projects.languages[0] }}</p>
+            <p class="live-demo" v-if="projects.demolink">
+              {{ projects.demolink }}
+            </p>
+            <a
+              class="github-link"
+              :href="`${projects.githublink}`"
+              target="_blank"
+            >
+              <span>View on Github</span>
+              <img src="../assets/images/GitHub-Mark-Light-64px.png" />
+            </a>
           </span>
         </div>
         <p>{{ projects.name }}</p>
@@ -29,28 +40,62 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-
 export default {
-  // TODO: host information locally instead of using the store
-  computed: {
-    ...mapState(["projectdata"]),
+  data() {
+    return {
+      projects: [
+        {
+          name: "Old Portfolio Website",
+          languages: ["React"],
+          img: require("@/assets/images/old-portfolio-website.png"),
+          githublink: "https://github.com/cmkozak/personal-website",
+          demolink: "",
+          inprogress: false,
+          expanded: false,
+          info: `In my previous portfolio, my goal was to expand my knowledge on React and Javascript by creating a single page website that features a large amount of animations and transitions.<br/>
+      Before starting this website I had never used React before but I knew that I wanted to make my portfolio interactive so I started learning React a few months prior to creating this website.<br/>
+      I eventaully scrapped the project in early 2019.`,
+        },
+        {
+          name: "Currency Converter",
+          languages: ["Javascript"],
+          img: require("@/assets/images/currency-converter.png"),
+          githublink: "https://github.com/cmkozak/currency-converter",
+          demolink: "",
+          inprogress: false,
+          expanded: false,
+          info: `"I created this project mostly to familiarize myself with using HTTP GET requests. Creating a simple single-page website that converts from 12 different currencies types to many different currencies used all over the world.<br/>
+          The website I use to get the information from is http://www.floatrates.com/json-feeds.html which offers free currency converters from many different countries updated daily.</br>
+          The project doesn’t look very pretty and I didn’t spend a lot of time on the design parts of it. I mostly just wanted to quickly throw some design together and get used to utilizing API requests."`,
+        },
+        {
+          name: "New Portfolio Website",
+          languages: ["Vue"],
+          img: "",
+          githublink: "https://github.com/cmkozak/new-personal-website",
+          demolink: "",
+          inprogress: true,
+          expanded: false,
+          info: `""`,
+        },
+      ],
+    };
   },
   methods: {
     expandCard(index) {
       // TODO: Need better logic here
       let cardElements = document.getElementsByClassName("card");
-      for (let i = 0; i < this.projectdata.projects.length; i++) {
+      for (let i = 0; i < this.projects.length; i++) {
         if (
           cardElements[i].id == index &&
           !cardElements[i].classList.contains("active")
         ) {
           cardElements[i].classList.add("active");
-          this.$store.state.projectdata.projects[i].expanded = true;
+          this.projects[i].expanded = true;
         } else if (cardElements[i].classList.contains("active")) {
           cardElements[i].classList.remove("active");
           cardElements[i].classList.add("active-fadeout");
-          this.$store.state.projectdata.projects[i].expanded = false;
+          this.projects[i].expanded = false;
         }
       }
     },
@@ -80,7 +125,7 @@ export default {
   -o-transition: all 1s ease-in-out;
   transition: all 1s ease-in-out;
   * {
-    background-color: rgb(46, 46, 46) !important;
+    background-color: rgb(46, 46, 46);
   }
   img {
     margin-left: 10px;
@@ -117,6 +162,10 @@ export default {
   -moz-transition: all 1s ease-in-out;
   -o-transition: all 1s ease-in-out;
   transition: all 1s ease-in-out;
+  .content {
+    display: block;
+    padding-left: 8px;
+  }
   .top-card-content {
     display: flex;
     justify-content: space-between;
@@ -129,7 +178,32 @@ export default {
     }
     .side {
       vertical-align: center;
+      margin-left: auto;
+      margin-right: auto;
     }
+  }
+}
+
+.github-link {
+  display: inline-block;
+  background-color: black;
+  padding: 7px 7px 7px 7px;
+  border-radius: 20px;
+  align-content: center;
+  text-align: center;
+  color: #fff;
+  * {
+    background-color: black;
+  }
+  span {
+    display: inline-block;
+  }
+  img {
+    width: 32px;
+    height: 32px;
+    display: inline-block;
+    vertical-align: middle;
+    margin: 0 0 0 10px;
   }
 }
 </style>
